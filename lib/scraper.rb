ac9530aefc2ff45215f4276b1d2c49bed1819e14
GraceOyiza <graceoyiza13@gmail.com>
2020-09-21 16:19:15 +0100

    Add initialize

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,10 @@ require 'byebug'
 
 class Electronic
 
-    
+    def initialize
+        @url = 'https://www.ebay.com/b/Samsung/bn_21834655'
+        @unparsed_page = HTTParty.get(@url)
+        @parsed_page =  Nokogiri::HTML(@unparsed_page)
+      end
 
 end
