4da6ec5538c3f4f325e6608b87cfb811a118f222
GraceOyiza <graceoyiza13@gmail.com>
2020-09-21 19:39:44 +0100

    Add count variable

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,25 +3,25 @@ require 'nokogiri'
 require 'byebug'
 
 class Electronic
-    attr_reader :url, :unparsed_page, :parsed_page
-    
+    attr_reader :count, :titles
+  
     def initialize
-        @url = 'https://www.ebay.com/b/Samsung/bn_21834655'
-        @unparsed_page = HTTParty.get(@url)
-        @parsed_page =  Nokogiri::HTML(@unparsed_page)
+      @url = 'https://www.ebay.com/b/Samsung/bn_21834655'
+      @unparsed_page = HTTParty.get(@url)
+      @parsed_page =  Nokogiri::HTML(@unparsed_page)
+      @count = 0
+    end
+  
+    def scrape
+      @titles = parsed_page.css('h3.s-item__title')
+      prices = parsed_page.css('span.s-item__price')
+      @count = titles.length
+      
+     puts "#{@count} items found" 
+      
+     
+      titles.each_with_index do |item, index|
+        puts "#{item.text} -  #{prices[index].text}"
       end
-
-      def scrape
-
-        titles = parsed_page.css('h3.s-item__title')
-        prices = parsed_page.css('span.s-item__price')
-
-        puts "#{titles.length} items found" 
-
-        titles.each_with_index do |item, index|
-            puts "#{item.text} -  #{prices[index].text}"
-        end
-
-      end
-
-end
+    end
+  end
