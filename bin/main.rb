ec74883a5b885f7c501b5a96232610ddc95076f9
GraceOyiza <graceoyiza13@gmail.com>
2020-09-23 12:41:15 +0100

    Fixed rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -5,13 +5,10 @@ require_relative '../lib/scraper.rb'
 electronic = Phone.new
 electronic.scrape
 
-
-
 puts "#{electronic.count} items found"
 
 electronic.titles.each_with_index do |item, index|
-    puts "#{item.text} ------------------  #{electronic.prices[index].text}"
-    puts "***********************************************************************************************************************************"
-    puts ''
+  puts "#{item.text} ------------------  #{electronic.prices[index].text}"
+  puts '**************************************************************************************************************'
+  puts ''
 end
-
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,6 @@
 require 'httparty'
 require 'nokogiri'
 
-
 class Phone
   attr_reader :count, :titles, :prices
 
@@ -16,7 +15,5 @@ class Phone
     @titles = @parsed_page.css('h3.s-item__title')
     @prices = @parsed_page.css('span.s-item__price')
     @count = titles.length
-
-    
   end
 end
